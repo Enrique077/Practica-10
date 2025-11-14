f432e8fa3df3548489a7f12a0f15cfe3438348ad
Enrique077 <enriquemena.xs@gmail.com>
2025-11-14 15:14:51 -0600

    ejercicio2a.c

--- ejercicio2a.c
+++ ejercicio2a.c
@@ -1,2 +1,17 @@
 /*Enrique Mena Hernández
 programa 2a*/
+#include<stdio.h>
+int main()
+{
+ int i,j,a[5][5];
+ for (i=0 ; i<5 ; i++)//Representa al renglón del arreglo
+ {
+    for (j=0 ; j<5 ; j++)//Representa a la columna del arreglo
+    {
+        a[i][j]=i+j;
+        printf("\t%d, ",a[i][j]);
+    }
+    printf("\n");
+ }
+ return 0;
+}
